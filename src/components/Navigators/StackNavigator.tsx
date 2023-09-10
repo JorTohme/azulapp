43d9e16d18380119ba30ba7837e107b69b06ff0c
Jorge <jorlopeztohme@gmail.com>
2023-09-10 15:35:29 -0300

    [wip]

--- src/components/Navigators/StackNavigator.tsx
+++ src/components/Navigators/StackNavigator.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import TabNavigator from './TabNavigator';
 import Login from '../UserHandling/Login';
+import OptionsMenu from '../../Views/OptionsMenu';
+import Colors from '../../utils/Colors';
 
 const Stack = createNativeStackNavigator();
 
@@ -14,6 +16,18 @@ export default function StackNavigator() {
       }}>
       <Stack.Screen name="Login" component={Login} />
       <Stack.Screen name="Main" component={TabNavigator} />
+      <Stack.Screen
+        name="Options"
+        component={OptionsMenu}
+        options={{
+          headerShown: true,
+          headerStyle: {
+            backgroundColor: Colors.selected,
+          },
+          headerTitle: '',
+          headerTintColor: '#fff',
+        }}
+      />
     </Stack.Navigator>
   );
 }
